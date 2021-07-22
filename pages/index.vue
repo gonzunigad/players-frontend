d6e42d12a8d649ab691bc63bea30ab861ccb445e
Gonzalo De Spírito <gonzunigad@gmail.com>
2021-07-21 20:50:01 -0400

    Improve UI

--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,20 @@
 <template>
   <div>
-    <div class="flex bg-white w-1/2 rounded shadow-lg mx-auto mt-20 p-5">
-      <div class="search">
-        <input v-model="searchText" ref="searchBox" class="p-2 text-gray-600 outline-none" type="text" placeholder="Buscar jugador" @keyup="search">
-      </div>
+    <div class="flex bg-white w-1/2 rounded shadow-lg mx-auto mt-20">
+        <input v-model="searchText" ref="searchBox" class="p-5 rounded text-gray-600 w-full outline-none" type="text" placeholder="Buscar jugador" @keyup="search">
     </div>
 
     <div v-if="players.length > 0" class="w-3/4 mx-auto mt-5 p-5">
       <!--<h3 class="bold text-2xl mb-20">Jugadores</h3>-->
       <div class="players grid grid-cols-2 md:grid-cols-4 gap-4">
-        <div v-for="player in players" class="flex w-full mt-10 flex-col bg-white rounded shadow-lg mx-auto p-2">
+        <div v-for="player in players" class="flex text-center w-full mt-10 flex-col bg-white rounded shadow-lg mx-auto p-2">
           <img class="w-20 -top-10 mx-auto relative" :alt="player.nickname" width="100" height="100" :src="'https://' + player.avatar" />
-          <span class="-mt-5 font-bold">Nick: {{ player.nickname }}</span>
-          <span>Status: {{ player.status }}</span>
-          <span>Balance: {{ formatNumber(player.balance) }}</span>
-          <span class="text-gray-600 ml-auto italic text-sm">ID: {{ player.id }}</span>
+          <span class="-mt-8 text-xl font-light">{{ player.nickname }}</span>
+          <span class=" mb-5 text-sm">{{ player.status }}</span>
+          <div class="w-full flex">
+            <span class="text-gray-600 mr-auto italic text-sm">Balance: {{ formatNumber(player.balance) }}</span>
+            <span class="text-gray-600 ml-auto italic text-sm">#{{ player.id }}</span>
+          </div>
         </div>
       </div>
     </div>
